62d89e8e7533fe4e6bafa6f84ce4a303bb4cc587
xuyaqist <xuyaqist@gmail.com>
2026-06-05 10:22:58 +0800

    Revert "init"
    This reverts commit 8e24e7630314f7eb0690abb9cbda33ec7f660f67.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AgentManager } from "@/components/agent-manager"
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-background">
-      <AgentManager />
-    </main>
-  )
-}
